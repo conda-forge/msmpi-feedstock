bb7d4b322cf40a9cafb5bbe3026464e9c958571e
Leo Fang <leofang@bnl.gov>
2021-04-23 17:05:52 -0400

    [WIP] switch to official installer

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,24 +1,38 @@
-if "%ARCH%"=="32" (
-    set PLATFORM=Win32
-) else (
-    set PLATFORM=x64
-)
+if not exist %LIBRARY_BIN% mkdir %LIBRARY_BIN% || exit 1
+if not exist %LIBRARY_LIB% mkdir %LIBRARY_LIB% || exit 1
+if not exist %LIBRARY_INC% mkdir %LIBRARY_INC% || exit 1
 
-msbuild.exe /p:Platform=%PLATFORM% /p:Configuration=Release
+start "Install MS-MPI SDK" /wait msiexec.exe /quiet /qn /i msmpisdk.msi || exit 1
 
-for /r %SRC_DIR%\out\Release-%PLATFORM% %%f in (*.exe) do @copy "%%f" %LIBRARY_BIN%
-for /r %SRC_DIR%\out\Release-%PLATFORM% %%f in (*.dll) do @copy "%%f" %LIBRARY_BIN%
-for /r %SRC_DIR%\out\Release-%PLATFORM% %%f in (*.lib) do @copy "%%f" %LIBRARY_LIB%
-for /r %SRC_DIR%\out\Release-%PLATFORM% %%f in (*.f90) do @copy "%%f" %LIBRARY_INC%
-for /r %SRC_DIR%\out\Release-%PLATFORM% %%f in (*.h) do @copy "%%f" %LIBRARY_INC%
+dir /s /b
 
-rem ensure the correct header for the platform is added 
-if "%ARCH%"=="32" (
-    copy %SRC_DIR%\out\Release-x64\bin\sdk\inc\x86\mpifptr.h %LIBRARY_INC%\mpifptr.h
-) else (
-    copy %SRC_DIR%\out\Release-x64\bin\sdk\inc\x64\mpifptr.h %LIBRARY_INC%\mpifptr.h
-)
+mkdir xxxxx
+start "Install MS-MPI Runtime" /wait msmpisetup.exe -unattend -installroot %cd%\xxxxx -verbose
 
+dir /s /b xxxxx
+
+exit 1
+:: if "%ARCH%"=="32" (
+::     set PLATFORM=Win32
+:: ) else (
+::     set PLATFORM=x64
+:: )
+:: 
+:: msbuild.exe /p:Platform=%PLATFORM% /p:Configuration=Release
+:: 
+:: for /r %SRC_DIR%\out\Release-%PLATFORM% %%f in (*.exe) do @copy "%%f" %LIBRARY_BIN%
+:: for /r %SRC_DIR%\out\Release-%PLATFORM% %%f in (*.dll) do @copy "%%f" %LIBRARY_BIN%
+:: for /r %SRC_DIR%\out\Release-%PLATFORM% %%f in (*.lib) do @copy "%%f" %LIBRARY_LIB%
+:: for /r %SRC_DIR%\out\Release-%PLATFORM% %%f in (*.f90) do @copy "%%f" %LIBRARY_INC%
+:: for /r %SRC_DIR%\out\Release-%PLATFORM% %%f in (*.h) do @copy "%%f" %LIBRARY_INC%
+:: 
+:: rem ensure the correct header for the platform is added 
+:: if "%ARCH%"=="32" (
+::     copy %SRC_DIR%\out\Release-x64\bin\sdk\inc\x86\mpifptr.h %LIBRARY_INC%\mpifptr.h
+:: ) else (
+::     copy %SRC_DIR%\out\Release-x64\bin\sdk\inc\x64\mpifptr.h %LIBRARY_INC%\mpifptr.h
+:: )
+ 
 setlocal EnableDelayedExpansion
 
 :: Copy the [de]activate scripts to %PREFIX%\etc\conda\[de]activate.d.
