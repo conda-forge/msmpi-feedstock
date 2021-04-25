b7ad9856dcfd0a6489dac5ca0c71b5097565c40b
Leo Fang <leofang@bnl.gov>
2021-04-25 14:16:19 -0400

    Switch to official installer (#6)
    * [WIP] switch to official installer
    
    * MNT: Re-rendered with conda-build 3.21.4, conda-smithy 3.10.1, and conda-forge-pinning 2021.04.23.13.32.29
    
    * fix double downloads
    
    * track where the file is installed
    
    * set TARGETDIR; print install progress
    
    * try admin install
    
    * check where error happens
    
    * another shot
    
    * check
    
    * force
    
    * try
    
    * debug print
    
    * revert
    
    * try
    
    * Update bld.bat
    
    * try fixing path
    
    * add 7z
    
    * use 7z to extract
    
    * typo
    
    * overhaul
    
    * debug
    
    * fix lib path
    
    * update license files
    
    MNT: Re-rendered with conda-build 3.20.3, conda-smithy 3.10.1, and conda-forge-pinning 2021.04.24.06.28.24
    
    * remove redundant hierarchy
    
    * remove invalid fortran lib check
    
    * Delete conda-build.patch
    
    * build 10.1.1
    
    * fix sha256
    
    * try pingpong
    
    * fix delimiter
    
    * fix test file path
    
    * force re-install to see if tests can be run
    
    * add one more test
    
    * fix typo...
    
    * clean up build script
    
    * add mpi4py to tests
    
    * fix pythonn
    
    * pip
    
    * add cython
    
    * fix pip install; check existing MS-MPI components
    
    * disable search
    
    * one last shot...
    
    * patch MSMPI_VER
    
    * fix
    
    * check return value
    
    * make silly mistake on purpose
    
    * Restore!
    
    * try to search for MPI program name for later removal
    
    * try uninstalling old MPI and avoiding installing a system-wide one
    
    * undo removing old MPI; move all test files under Tests
    
    * add a C test
    
    * remove Python test deps
    
    * fix copy
    
    * fix output and linker error
    
    Co-authored-by: conda-forge-linter <github-actions@email.com>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,31 +1,77 @@
-if "%ARCH%"=="32" (
-    set PLATFORM=Win32
-) else (
-    set PLATFORM=x64
-)
-
-msbuild.exe /p:Platform=%PLATFORM% /p:Configuration=Release
-
-for /r %SRC_DIR%\out\Release-%PLATFORM% %%f in (*.exe) do @copy "%%f" %LIBRARY_BIN%
-for /r %SRC_DIR%\out\Release-%PLATFORM% %%f in (*.dll) do @copy "%%f" %LIBRARY_BIN%
-for /r %SRC_DIR%\out\Release-%PLATFORM% %%f in (*.lib) do @copy "%%f" %LIBRARY_LIB%
-for /r %SRC_DIR%\out\Release-%PLATFORM% %%f in (*.f90) do @copy "%%f" %LIBRARY_INC%
-for /r %SRC_DIR%\out\Release-%PLATFORM% %%f in (*.h) do @copy "%%f" %LIBRARY_INC%
-
-rem ensure the correct header for the platform is added 
-if "%ARCH%"=="32" (
-    copy %SRC_DIR%\out\Release-x64\bin\sdk\inc\x86\mpifptr.h %LIBRARY_INC%\mpifptr.h
-) else (
-    copy %SRC_DIR%\out\Release-x64\bin\sdk\inc\x64\mpifptr.h %LIBRARY_INC%\mpifptr.h
-)
-
 setlocal EnableDelayedExpansion
 
-:: Copy the [de]activate scripts to %PREFIX%\etc\conda\[de]activate.d.
-:: This will allow them to be run on environment activation.
+if not exist %LIBRARY_BIN% mkdir %LIBRARY_BIN% || exit 1
+if not exist %LIBRARY_LIB% mkdir %LIBRARY_LIB% || exit 1
+if not exist %LIBRARY_INC% mkdir %LIBRARY_INC% || exit 1
+
+:: Even if we do this it still doesn't help to fix the PMP version mismatch error
+:: (https://github.com/conda-forge/msmpi-feedstock/issues/2), so we rely on the
+:: installer to force updating it for testing...
+:: echo "check installed programs..."
+:: wmic product get name  || exit 1
+:: echo "remove MPI from the image..."
+:: wmic product where name="Microsoft MPI (7.1.12437.25)" call uninstall || exit 1
+
+echo "check pwd..."
+dir /s /b
+
+mkdir temp
+mkdir License
+echo "Installing MS-MPI SDK..."
+:: note: it has to be /a, not /i
+msiexec.exe /quiet /qn /a "%cd%\msmpisdk.msi" TARGETDIR="%cd%\temp" || exit 1
+
+move "%cd%\temp\PFiles\Microsoft SDKs\MPI\Lib\x64\*.lib" %LIBRARY_LIB% || exit 1
+move "%cd%\temp\PFiles\Microsoft SDKs\MPI\Include\*.h" %LIBRARY_INC% || exit 1
+move "%cd%\temp\PFiles\Microsoft SDKs\MPI\Include\*.f90" %LIBRARY_INC% || exit 1
+move "%cd%\temp\PFiles\Microsoft SDKs\MPI\Include\x64\*.h" %LIBRARY_INC% || exit 1
+move "%cd%\temp\PFiles\Microsoft SDKs\MPI\License\*" "%cd%\License" || exit 1
+
+echo "check pwd..."
+dir /s /b
+echo "check target dir..."
+dir /s /b "%cd%\temp"
+rmdir /q /s temp || exit 1
+
+mkdir temp
+mkdir Tests
+echo "Installing MS-MPI Runtime..."
+:: this does not work because it insists on installing to C:\Program Files\Microsoft MPI\, not to our custom path;
+:: however, we still need this to overwrite the vm image's built-in installation so that we can run tests
+"%cd%\msmpisetup.exe" -unattend -force -full -installroot "%cd%\temp" -verbose -log "%cd%\log.txt" || exit 1
+echo "printing log..."
+type "%cd%\log.txt" || exit 1
+del log.txt || exit 1
+:: this extraction does the real work for the purpose of packaging
+7z x msmpisetup.exe -o"%cd%\temp" || exit 1
+
+move "%cd%\temp\*.dll" %LIBRARY_BIN% || exit 1
+move "%cd%\temp\mpiexec.exe" %LIBRARY_BIN% || exit 1
+move "%cd%\temp\mpitrace.man" %LIBRARY_BIN% || exit 1
+move "%cd%\temp\msmpilaunchsvc.exe" %LIBRARY_BIN% || exit 1
+move "%cd%\temp\smpd.exe" %LIBRARY_BIN% || exit 1
+move "%cd%\temp\*.exe" "%cd%\Tests" || exit 1
+move "%cd%\temp\*" "%cd%\License" || exit 1
+
+echo "checking installroot..."
+dir /s /b "%cd%\temp"
+rmdir "%cd%\temp" || exit 1
+:: note: conda-build would copy the two folders below for us
+echo "checking License..."
+dir /s /b "%cd%\License"
+echo "checking Tests..."
+dir /s /b "%cd%\Tests"
+
+echo "copy the [de]activate scripts..."
 for %%F in (activate deactivate) DO (
     if not exist %PREFIX%\etc\conda\%%F.d mkdir %PREFIX%\etc\conda\%%F.d
-    copy %RECIPE_DIR%\%%F.bat %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat
+    copy %RECIPE_DIR%\%%F.bat %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat || exit 1
 )
 
+echo "patching mpi.h..."
+patch "%LIBRARY_INC%\mpi.h" "%RECIPE_DIR%\MSMPI_VER.diff" || exit 1
+copy "%RECIPE_DIR%\tests\*" .\Tests || exit 1
+
+echo "checking source dir..."
 dir /s /b
+echo "DONE!"
---
+++ recipe/tests/get_mpi_ver.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main() {
+  printf("%d", MSMPI_VER);
+  return 0;
+}
