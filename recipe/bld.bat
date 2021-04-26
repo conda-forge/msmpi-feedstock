e9d2d79cb098b8676b14d74a4a73e52681e363ee
Leo Fang <leofang@bnl.gov>
2021-04-25 20:24:48 -0400

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -12,10 +12,11 @@ if not exist %LIBRARY_INC% mkdir %LIBRARY_INC% || exit 1
 :: echo "remove MPI from the image..."
 :: wmic product where name="Microsoft MPI (7.1.12437.25)" call uninstall || exit 1
 
-:: echo "check registry..."
+echo "check registry..."
 :: REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\HPC" || exit 1
-echo "hunt down smpd..."
-tasklist /v  
+REG QUERY "HKEY_CURRENT_USER\Software\Microsoft\MPI" || exit 1
+:: echo "hunt down smpd..."
+:: tasklist /v  
 exit 1
 
 echo "check pwd..."
