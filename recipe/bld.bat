ef5874d8834155f3f6b8e41c2832dc9789d8e987
Leo Fang <leofang@bnl.gov>
2021-04-28 15:46:28 -0400

    take advantage of -installroot

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,8 +5,8 @@ if not exist %LIBRARY_LIB% mkdir %LIBRARY_LIB% || exit 1
 if not exist %LIBRARY_INC% mkdir %LIBRARY_INC% || exit 1
 
 :: Even if we do this it still doesn't help to fix the PMP version mismatch error
-:: (https://github.com/conda-forge/msmpi-feedstock/issues/2), so we rely on the
-:: installer to force updating it for testing...
+:: (https://github.com/conda-forge/msmpi-feedstock/issues/2). But, this will make
+:: -installroot work later!
 :: echo "check installed programs..."
 :: wmic product get name  || exit 1
 echo "remove MPI from the image..."
@@ -17,6 +17,7 @@ dir /s /b
 
 mkdir temp
 mkdir License
+mkdir Tests
 echo "Installing MS-MPI SDK..."
 :: note: it has to be /a, not /i
 msiexec.exe /quiet /qn /a "%cd%\msmpisdk.msi" TARGETDIR="%cd%\temp" || exit 1
@@ -33,28 +34,20 @@ echo "check target dir..."
 dir /s /b "%cd%\temp"
 rmdir /q /s temp || exit 1
 
-mkdir temp
-mkdir Tests
 echo "Installing MS-MPI Runtime..."
-:: this installs to C:\Program Files\Microsoft MPI\ instead of to our custom path (-installroot);
-:: however, we still need this to
-:: 1. overwrite the vm image's built-in installation so that we can run tests
-:: 2. generate the correct dlls (yes, the installer would write to the dlls!)
-"%cd%\msmpisetup.exe" -unattend -force -full -installroot "%cd%\temp" -verbose -log "%cd%\log.txt" || exit 1
+:: We must run (not extract!) the installer to generate the correct dlls (yes, it would write to the dlls!)
+"%cd%\msmpisetup.exe" -unattend -force -full -installroot "%PREFIX%\Library" -verbose -log "%cd%\log.txt" || exit 1
 echo "printing log..."
 type "%cd%\log.txt" || exit 1
 del log.txt || exit 1
 
 move "C:\Windows\System32\msmpi.dll" %LIBRARY_BIN% || exit 1
 move "C:\Windows\System32\msmpires.dll" %LIBRARY_BIN% || exit 1
-dir "C:\Program Files\Microsoft MPI\" || exit 1
-move "C:\Program Files\Microsoft MPI\Bin\*" %LIBRARY_BIN% || exit 1
-move "C:\Program Files\Microsoft MPI\Benchmarks\*" "%cd%\Tests" || exit 1
-move "C:\Program Files\Microsoft MPI\License\*" "%cd%\License" || exit 1
+move "%PREFIX%\Library\Benchmarks\*" "%cd%\Tests" || exit 1
+rmdir "%PREFIX%\Library\Benchmarks" || exit 1
+move "%PREFIX%\Library\License\*" "%cd%\License" || exit 1
+rmdir "%PREFIX%\Library\License" || exit 1
 
-echo "checking installroot..."
-dir /s /b "%cd%\temp"
-rmdir "%cd%\temp" || exit 1
 :: note: conda-build would copy the two folders below for us
 echo "checking License..."
 dir /s /b "%cd%\License"
