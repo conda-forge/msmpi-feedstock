39f9b2a4c565499f956bab793ad5a5257383fdb7
Isuru Fernando <isuruf@gmail.com>
2021-06-10 11:24:56 -0500

    Add a warning about system msmpi

--- recipe/activate.bat
+++ recipe/activate.bat
@@ -2,6 +2,14 @@ if defined CONDA_BUILD_STATE (
     @echo on
 )
 
+if exist "C:\Windows\System32\msmpi.dll" (
+    echo "You seem to have a system wide installation of MSMPI. "
+    echo "Due to the way DLL loading works on windows, system wide installation "
+    echo "will probably overshadow the conda installation. Uninstalling "
+    echo "the system wide installation and forced deleting C:\Windows\System32\msmpi*.dll"
+    echo "will help, but may break other software using the system wide installation."
+)
+
 :: Backup environment variables (only if the variables are set)
 if defined MSMPI_BIN (
     set "MSMPI_BIN_CONDA_BACKUP=%MSMPI_BIN%"
