34785c2c20200fd5da638f154ac1933fdde8cac3
nicogodet <nicolas.godet@outlook.fr>
2023-10-22 18:27:25 +0200

    Add wrappers

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,9 +5,9 @@ if "%ARCH%"=="32" (
 )
 
 msbuild.exe /p:Platform=%PLATFORM% /p:Configuration=Release
+if errorlevel 1 exit 1
 
 for /r %SRC_DIR%\out\Release-%PLATFORM% %%f in (*.exe) do @copy "%%f" %LIBRARY_BIN%
-for /r %SRC_DIR%\out\Release-%PLATFORM% %%f in (*.dll) do @copy "%%f" %LIBRARY_BIN%
 for /r %SRC_DIR%\out\Release-%PLATFORM% %%f in (*.lib) do @copy "%%f" %LIBRARY_LIB%
 for /r %SRC_DIR%\out\Release-%PLATFORM% %%f in (*.f90) do @copy "%%f" %LIBRARY_INC%
 for /r %SRC_DIR%\out\Release-%PLATFORM% %%f in (*.h) do @copy "%%f" %LIBRARY_INC%
@@ -19,6 +19,46 @@ if "%ARCH%"=="32" (
     copy %SRC_DIR%\out\Release-x64\bin\sdk\inc\x64\mpifptr.h %LIBRARY_INC%\mpifptr.h
 )
 
+echo "patching mpi.h..."
+:: add --binary to handle the CRLF line ending
+patch --binary "%LIBRARY_INC%\mpi.h" "%RECIPE_DIR%\MSMPI_VER.diff" || exit 1
+if errorlevel 1 exit 1
+
+echo "Building wrappers..."
+mkdir build
+mkdir build\bin
+mkdir build\include
+mkdir build\lib
+
+cd build
+copy %LIBRARY_INC%\mpi.h include\
+copy %LIBRARY_INC%\mpif.h include\
+copy %LIBRARY_INC%\mpi.f90 include\
+copy %LIBRARY_INC%\mpifptr.h include\
+
+dlltool -k -d %SRC_DIR%\src\msys2\msmpi.def -l bin\msmpi.dll
+if errorlevel 1 exit 1
+
+set mpic=%SRC_DIR%\src\msys2\mpi.c
+set cflags=-s -O2 -DNDEBUG
+
+gcc %cflags% -o bin\mpicc.exe -DCC %mpic%
+if errorlevel 1 exit 1
+gcc %cflags% -o bin\mpicxx.exe -DCXX %mpic%
+if errorlevel 1 exit 1
+copy bin\mpicxx.exe "bin\mpic++.exe"
+gcc %cflags% -o bin\mpifort.exe -DFC %mpic%
+if errorlevel 1 exit 1
+copy bin\mpifort.exe bin\mpif77.exe
+copy bin\mpifort.exe bin\mpif90.exe
+bin\mpifort -c -Jinclude .\include\mpi.f90
+if errorlevel 1 exit 1
+del mpi.o
+if errorlevel 1 exit 1
+
+cd ..
+xcopy /e/y build %LIBRARY_PREFIX%
+
 setlocal EnableDelayedExpansion
 
 :: Copy the [de]activate scripts to %PREFIX%\etc\conda\[de]activate.d.
@@ -27,9 +67,5 @@ for %%F in (activate deactivate) DO (
     if not exist %PREFIX%\etc\conda\%%F.d mkdir %PREFIX%\etc\conda\%%F.d
     copy %RECIPE_DIR%\%%F.bat %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat || exit 1
 )
-
-echo "patching mpi.h..."
-:: add --binary to handle the CRLF line ending
-patch --binary "%LIBRARY_INC%\mpi.h" "%RECIPE_DIR%\MSMPI_VER.diff" || exit 1
 
 dir /s /b
